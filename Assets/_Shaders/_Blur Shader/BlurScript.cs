065274d75296aa4f83da3c98f47997f2113369d3
kingkrows <eiccheghrui@gmail.com>
2020-05-12 00:33:51 +0600

    added blur shader

---
+++ Assets/_Shaders/_Blur Shader/BlurScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlurScript : MonoBehaviour
+{
+    [Header("Put shader material here")]
+    public Material blurMaterial;
+    public float blurAmount=0f;
+
+    private bool _isBlurActive = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _isBlurActive = !_isBlurActive;
+            
+        }
+
+        float blurSpeed = 5f;
+
+        if (_isBlurActive)
+        {
+            blurAmount += blurSpeed * Time.deltaTime;
+            
+        }
+        else
+        {
+            blurAmount -= blurSpeed * Time.deltaTime;
+        }
+
+        blurAmount = Mathf.Clamp(blurAmount, 0f, 5f);
+        blurMaterial.SetFloat("_BlurAmount", blurAmount);
+
+    }
+
+
+
+}
